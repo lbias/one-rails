05620502c29f6b3a555e5465afc6d3115bdc94b7
lbias <306508354@qq.com>
2017-09-11 10:10:01 +0800

    controller, view jobs#edit, update

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -21,9 +21,22 @@ class JobsController < ApplicationController
     end
   end
 
+  def edit
+    @job = Job.find(params[:id])
+  end
+
+  def update
+    @job = Job.find(params[:id])
+    if @job.update(job_params)
+      redirect_to jobs_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def job_params
     params.require(:job).permit(:title, :description)
-  end  
+  end
 end
