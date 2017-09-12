fe51de9c5ea3120748892a7603f72e92c3e29a23
lbias <306508354@qq.com>
2017-09-12 09:43:22 +0800

    add wage_upper_bound, wage_lower_bound, contact_email to jobs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912013056) do
+ActiveRecord::Schema.define(version: 20170912014123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,9 @@ ActiveRecord::Schema.define(version: 20170912013056) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "wage_upper_bound"
+    t.integer "wage_lower_bound"
+    t.string "contact_email"
   end
 
   create_table "users", force: :cascade do |t|
