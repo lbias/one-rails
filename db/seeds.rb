14dc42e301324f5ef2f197fd0ef58deaac0671f8
lbias <306508354@qq.com>
2017-09-12 23:39:56 +0800

    create test data at seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Create 10 public jobs, and 10 hidden jobs"
+
+User.create([email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678', is_admin: 'true'])
+puts "Admin account created."
+
+create_jobs = for i in 1..10 do
+  Job.create!([title: "Job no.#{i}", description: "This is public job No. #{i}", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false"])
+end
+puts "10 Public jobs created."
+
+create_jobs = for i in 1..10 do
+  Job.create!([title: "Job no.#{i+10}", description: "This is hidden job No. #{i+10}", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true"])
+end
+puts "10 Hidden jobs created."
