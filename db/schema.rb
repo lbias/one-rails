338ad02b91477ead115db43d457fad5a3f5268cc
lbias <306508354@qq.com>
2017-09-12 17:29:05 +0800

    model resume with user_id, job_id, content

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170912082209) do
+ActiveRecord::Schema.define(version: 20170912092752) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20170912082209) do
     t.integer "wage_lower_bound"
     t.string "contact_email"
     t.boolean "is_hidden", default: true
+  end
+
+  create_table "resumes", force: :cascade do |t|
+    t.integer "job_id"
+    t.integer "user_id"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
