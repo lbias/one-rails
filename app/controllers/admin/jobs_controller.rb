de56a999cf3ed2eff5aad568eff0b2645cdf6c5c
lbias <306508354@qq.com>
2017-09-12 16:58:43 +0800

    model jobs#publish, hide, controller admin::jobs#publish, hide

--- app/controllers/admin/jobs_controller.rb
+++ app/controllers/admin/jobs_controller.rb
@@ -44,6 +44,18 @@ class Admin::JobsController < ApplicationController
     redirect_to admin_jobs_path
   end
 
+  def publish
+    @job = Job.find(params[:id])
+    @job.publish!
+    redirect_to admin_jobs_path
+  end
+
+  def hide
+    @job = Job.find(params[:id])
+    @job.hide!
+    redirect_to admin_jobs_path
+  end
+
   private
   def job_params
     params.require(:job).permit(:title, :description, :wage_upper_bound, :wage_lower_bound, :contact_email,:is_hidden)
