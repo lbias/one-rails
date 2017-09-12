251ed12677ee203222c9a92481c7e72c4100f2ec
lbias <306508354@qq.com>
2017-09-12 16:29:26 +0800

    jobs descending order by created_at

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -2,7 +2,7 @@ class JobsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :update, :edit, :destroy]
 
   def index
-    @jobs = Job.all.where(:is_hidden => false)
+    @jobs = Job.all.where(:is_hidden => false).order("created_at DESC")
   end
 
   def show
