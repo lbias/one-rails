b50304d145ac9f14e7ac6ad013fe8098ddf565eb
lbias <306508354@qq.com>
2017-10-13 22:18:14 +0800

    limit the cart_item quantity equal or less than stocks

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -4,7 +4,12 @@ class CartItemsController < ApplicationController
   def update
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
-    @cart_item.update(cart_item_params)
+    if @cart_item.product.quantity >= cart_item_params[:quantity].to_i
+      @cart_item.update(cart_item_params)
+      flash[:notice] = "You updated the quantity successully."
+    else
+      flash[:warning] = "Insufficient inventory, please try with fewer quantity."
+    end
     redirect_to carts_path
   end
 
