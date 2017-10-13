baec888f88f1165fa0722b05755cff66688a61a9
lbias <306508354@qq.com>
2017-10-13 17:27:28 +0800

    controller products#add_to_cart

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,4 +6,10 @@ class ProductsController < ApplicationController
   def show
     @product = Product.find(params[:id])
   end
+
+  def add_to_cart
+    @product = Product.find(params[:id])
+    redirect_to @product
+    flash[:notice] = "test adding to cart"
+  end
 end
