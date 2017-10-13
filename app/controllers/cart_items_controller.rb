900c69264c41621e6afd0c8a4f111351ecd6406c
lbias <306508354@qq.com>
2017-10-13 22:00:06 +0800

    change product quantity in cart

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -1,6 +1,13 @@
 class CartItemsController < ApplicationController
   before_action :authenticate_user!
 
+  def update
+    @cart = current_cart
+    @cart_item = @cart.cart_items.find_by(product_id: params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to carts_path
+  end
+
   def destroy
     @cart = current_cart
     @cart_item = @cart.cart_items.find_by(product_id: params[:id])
@@ -8,5 +15,10 @@ class CartItemsController < ApplicationController
     @cart_item.destroy
     flash[:warning] = "You removed #{@product.title} from the cart successully."
     redirect_to @product
+  end
+
+  private
+  def cart_item_params
+    params.require(:cart_item).permit(:quantity)
   end
 end
